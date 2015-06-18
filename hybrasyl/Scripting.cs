24803dd40eccff8edeeebc7fbe8e94fd66ea1fcb
Luke Segars <luke@lukesegars.com>
2015-06-18 00:45:34 -0700

    Cleaning up some files.

--- hybrasyl/Scripting.cs
+++ hybrasyl/Scripting.cs
@@ -340,7 +340,6 @@ __builtins__.raw_input = None
             var paths = Engine.GetSearchPaths();
             // FIXME: obvious
             paths.Add(@"C:\Program Files (x86)\IronPython 2.7\Lib");
-			paths.Add(@"/usr/lib/python2.7");
             Engine.SetSearchPaths(paths);
             Engine.ImportModule("random");
 
