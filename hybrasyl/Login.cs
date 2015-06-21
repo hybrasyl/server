b5ebfa4c2a48d7ab8b6282f7453a385d6226115e
Luke Segars <luke@lukesegars.com>
2015-06-21 13:13:05 -0700

    Initial patch to support client-side password change requests.
    Handles opcode 0x26, which is the password change opcode. Also abstracts
    certain functions that were previously embedded in login and account
    creation methods but now need to be shared. New methods include:
    - HashPassword
    - VerifyPassword
    - ValidPassword
    Two particular questions are still open: I need to figure out what
    message type needs to be sent to the frontend (I'm currently copying "3"
    from elsewhere in the login server but have no clue what that means),
    and I also need to confirm that the strings I'm returning are correct.

--- hybrasyl/Login.cs
+++ hybrasyl/Login.cs
@@ -48,6 +48,7 @@ namespace Hybrasyl
             PacketHandlers[0x03] = PacketHandler_0x03_Login;
             PacketHandlers[0x04] = PacketHandler_0x04_CreateB;
             PacketHandlers[0x10] = PacketHandler_0x10_ClientJoin;
+            PacketHandlers[0x26] = PacketHandler_0x26_ChangePassword;
             PacketHandlers[0x4B] = PacketHandler_0x4B_RequestNotification;
             PacketHandlers[0x68] = PacketHandler_0x68_RequestHomepage;
         }
@@ -60,6 +61,10 @@ namespace Hybrasyl
             var password = packet.ReadString8();
             var email = packet.ReadString8();
 
+            // This string will contain a client-ready message if the provided password
+            // isn't valid.
+            string passwordErr = null;
+
             if (Game.World.PlayerExists(name))
             {
                 client.LoginMessage("That name is unavailable.", 3);
@@ -68,19 +73,14 @@ namespace Hybrasyl
             {
                 client.LoginMessage("Names must be between 4 to 12 characters long.", 3);
             }
-            else if (password.Length < 4 || password.Length > 8)
+            else if (!ValidPassword(password, name, out passwordErr))
             {
-                client.LoginMessage("Passwords must be between 4 and 8 characters long.", 3);
-            }
-            else if (name == password)
-            {
-                client.LoginMessage("Your password may not be the same as your username.", 3);
+                client.LoginMessage(passwordErr, 3);
             }
             else if (Regex.IsMatch(name, "^[A-Za-z]{4,12}$"))
             {
                 client.NewCharacterName = name;
-                client.NewCharacterPassword = BCrypt.Net.BCrypt.HashPassword(password,
-                    BCrypt.Net.BCrypt.GenerateSalt(12));
+                client.NewCharacterPassword = HashPassword(password);
                 client.LoginMessage("\0", 0);
             }
             else
@@ -108,7 +108,7 @@ namespace Hybrasyl
                 }
                 else
                 {
-                    if (BCrypt.Net.BCrypt.Verify(password, result.password_hash))
+                    if (VerifyPassword(password, result))
                     {
                         Logger.DebugFormat("cid {0}: password verified for {1}", client.ConnectionId, name);
 
@@ -241,6 +241,69 @@ namespace Hybrasyl
             }
 
         }
+        
+        private void PacketHandler_0x26_ChangePassword(Client client, ClientPacket packet)
+        {
+            var name = packet.ReadString8();
+            var currentPass = packet.ReadString8();
+            // Clientside validation ensures that the same string is typed twice for the new
+            // password, and the new password is only sent to the server once. We can assume
+            // that they matched if 0x26 request is sent from the client.
+            var newPass = packet.ReadString8();
+
+            using (var ctx = new hybrasylEntities(Constants.ConnectionString))
+            {
+                var player = ctx.players.Where(p => p.name == name).SingleOrDefault();
+
+                // Check that `name` exists. If not, return a message indicating that to the user.
+                if (player == null)
+                {
+                    // TODO(luke-segars): I'm not sure what the `type` param means. Dig into that.
+                    client.LoginMessage("No character with that name exists.", 3);
+                }
+                // If the player does exist, validate the current and new passwords before updating.
+                else
+                {
+                    // Check that the current password is correct and the new password is different
+                    // than the current password.
+                    if (VerifyPassword(currentPass, player))
+                    {
+                        if (!VerifyPassword(newPass, player))
+                        {
+                            // Check if the password is valid.
+                            string err = null;
+                            if (ValidPassword(newPass, player.name, out err))
+                            {
+                                player.password_hash = HashPassword(newPass);
+                                ctx.SaveChanges();
+
+                                // Let the user know the good news.
+                                client.LoginMessage("Password change successful!", 3);
+                            }
+                            else
+                            {
+                                client.LoginMessage(err, 3);
+                            }
+                        }
+                        else
+                        {
+                            client.LoginMessage("Your new password must be different than your old password.", 3);
+                        }
+                    }
+                    // The current password is incorrect. Don't allow any changes to happen.
+                    else
+                    {
+                        client.LoginMessage("Incorrect password.", 3);
+                    }
+
+                    Logger.DebugFormat("Player {0} changing password", name);
+                }
+                // Check that `currentPass` is their current password.
+                // Check that `currentPass` and `newPass` aren't the same.
+                // Store `newPass`
+            }
+        }
+        
         private void PacketHandler_0x4B_RequestNotification(Client client, ClientPacket packet)
         {
             var x60 = new ServerPacket(0x60);
@@ -256,6 +319,41 @@ namespace Hybrasyl
             x03.WriteByte(0x03);
             x03.WriteString8("http://www.hybrasyl.com");
             client.Enqueue(x03);
+        }
+
+        /**
+         * Hashes the provided password and returns the hashed version. This method should be used
+         * any time a raw password is being stored.
+         */
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+        }
+
+        private bool VerifyPassword(string password, player user)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, user.password_hash);
+        }
+
+        /**
+         * Checks that a string is a valid password.
+         */
+        private bool ValidPassword(string password, string name, out string msg)
+        {
+            if (password.Length < 4 || password.Length > 8)
+            {
+                msg = "Passwords must be between 4 and 8 characters long.";
+                return false;
+            }
+
+            if (password == name)
+            {
+                msg = "Your password may not be the same as your username.";
+                return false;
+            }
+
+            msg = null;
+            return true;
         }
     }
 }
