cc815af6b6ca57ec7c3e61d71edd988d6480c12a
Kyle Speck <kojasou@gmail.com>
2016-11-30 22:32:28 -0600

    Fix max level display for castables

---
+++ hybrasyl/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using Hybrasyl.Castables;
+using Hybrasyl.Objects;
+
+namespace Hybrasyl
+{
+    public static class Extensions
+    {
+        public static byte GetMaxLevelForUser(this Castable castable, User user)
+        {
+            if (user.IsMaster)
+            {
+                return 100;
+            }
+
+            switch (user.Class)
+            {
+                case Enums.Class.Peasant:
+                    return castable.MaxLevel.Peasant;
+                case Enums.Class.Warrior:
+                    return castable.MaxLevel.Warrior;
+                case Enums.Class.Rogue:
+                    return castable.MaxLevel.Rogue;
+                case Enums.Class.Wizard:
+                    return castable.MaxLevel.Wizard;
+                case Enums.Class.Priest:
+                    return castable.MaxLevel.Priest;
+                case Enums.Class.Monk:
+                    return castable.MaxLevel.Monk;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
