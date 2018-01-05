b460ffbe1c29e9cc768da0c9f1f6b67d601292d4
Michael Norris <norrismiv@gmail.com>
2018-01-04 21:08:49 -0500

    Move world controlhandlers and merchanthandlers into separate files. add in missing calls for merchant spell menus

---
+++ hybrasyl/WorldControlHandlers.cs
@@ -0,0 +1,283 @@
+﻿using Hybrasyl.Enums;
+using Hybrasyl.Objects;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrasyl
+{
+    public partial class World
+    {
+        #region Control Message Handlers
+
+        public void SetControlMessageHandlers()
+        {
+            ControlMessageHandlers[ControlOpcodes.CleanupUser] = ControlMessage_CleanupUser;
+            ControlMessageHandlers[ControlOpcodes.SaveUser] = ControlMessage_SaveUser;
+            ControlMessageHandlers[ControlOpcodes.ShutdownServer] = ControlMessage_ShutdownServer;
+            ControlMessageHandlers[ControlOpcodes.RegenUser] = ControlMessage_RegenerateUser;
+            ControlMessageHandlers[ControlOpcodes.LogoffUser] = ControlMessage_LogoffUser;
+            ControlMessageHandlers[ControlOpcodes.MailNotifyUser] = ControlMessage_MailNotifyUser;
+            ControlMessageHandlers[ControlOpcodes.StatusTick] = ControlMessage_StatusTick;
+            ControlMessageHandlers[ControlOpcodes.MonolithSpawn] = ControlMessage_SpawnMonster;
+            ControlMessageHandlers[ControlOpcodes.MonolithControl] = ControlMessage_MonolithControl;
+        }
+
+        private void ControlMessage_CleanupUser(HybrasylControlMessage message)
+        {
+            // clean up after a broken connection
+            var connectionId = (long)message.Arguments[0];
+            User user;
+            if (ActiveUsers.TryRemove(connectionId, out user))
+            {
+                Logger.InfoFormat("cid {0}: closed, player {1} removed", connectionId, user.Name);
+                if (user.ActiveExchange != null)
+                    user.ActiveExchange.CancelExchange(user);
+                ((IDictionary)ActiveUsersByName).Remove(user.Name);
+                user.Save();
+                user.UpdateLogoffTime();
+                user.Map?.Remove(user);
+                user.Group?.Remove(user);
+                Remove(user);
+                Logger.DebugFormat("cid {0}: {1} cleaned up successfully", user.Name);
+                DeleteUser(user.Name);
+            }
+        }
+
+        private void ControlMessage_RegenerateUser(HybrasylControlMessage message)
+        {
+            // regenerate a user
+            // USDA Formula for HP: MAXHP * (0.1 + (CON - Lv) * 0.01) <20% MAXHP
+            // USDA Formula for MP: MAXMP * (0.1 + (WIS - Lv) * 0.01) <20% MAXMP
+            // Regen = regen * 0.0015 (so 100 regen = 15%)
+            User user;
+            var connectionId = (long)message.Arguments[0];
+            if (ActiveUsers.TryGetValue(connectionId, out user))
+            {
+                uint hpRegen = 0;
+                uint mpRegen = 0;
+                double fixedRegenBuff = Math.Min(user.Regen * 0.0015, 0.15);
+                fixedRegenBuff = Math.Max(fixedRegenBuff, 0.125);
+                if (user.Hp != user.MaximumHp)
+                {
+                    hpRegen = (uint)Math.Min(user.MaximumHp * (0.1 * Math.Max(user.Con, (user.Con - user.Level)) * 0.01),
+                        user.MaximumHp * 0.20);
+                    hpRegen = hpRegen + (uint)(fixedRegenBuff * user.MaximumHp);
+                }
+                if (user.Mp != user.MaximumMp)
+                {
+                    mpRegen = (uint)Math.Min(user.MaximumMp * (0.1 * Math.Max(user.Int, (user.Int - user.Level)) * 0.01),
+                        user.MaximumMp * 0.20);
+                    mpRegen = mpRegen + (uint)(fixedRegenBuff * user.MaximumMp);
+                }
+                Logger.DebugFormat("User {0}: regen HP {1}, MP {2}", user.Name,
+                    hpRegen, mpRegen);
+                user.Hp = Math.Min(user.Hp + hpRegen, user.MaximumHp);
+                user.Mp = Math.Min(user.Mp + mpRegen, user.MaximumMp);
+                user.UpdateAttributes(StatUpdateFlags.Current);
+            }
+        }
+
+        private void ControlMessage_SaveUser(HybrasylControlMessage message)
+        {
+            // save a user
+            User user;
+            var connectionId = (long)message.Arguments[0];
+            if (ActiveUsers.TryGetValue(connectionId, out user))
+            {
+                Logger.DebugFormat("Saving user {0}", user.Name);
+                user.Save();
+            }
+            else
+            {
+                Logger.WarnFormat("Tried to save user associated with connection ID {0} but user doesn't exist",
+                    connectionId);
+            }
+        }
+
+        private void ControlMessage_ShutdownServer(HybrasylControlMessage message)
+        {
+            // Initiate an orderly shutdown
+            var userName = (string)message.Arguments[0];
+            Logger.WarnFormat("Server shutdown request initiated by {0}", userName);
+            // Chaos is Rising Up, yo.
+            foreach (var connection in ActiveUsers)
+            {
+                var user = connection.Value;
+                user.SendMessage("Chaos is rising up. Please re-enter in a few minutes.",
+                    Hybrasyl.MessageTypes.SYSTEM_WITH_OVERHEAD);
+            }
+
+            // Actually shut down the server. This terminates the listener loop in Game.
+            if (Game.IsActive())
+                Game.ToggleActive();
+
+            Logger.WarnFormat("Server has begun shutdown");
+        }
+
+        private void ControlMessage_LogoffUser(HybrasylControlMessage message)
+        {
+            // Log off the specified user
+            var userName = (string)message.Arguments[0];
+            Logger.WarnFormat("{0}: forcing logoff", userName);
+            User user;
+            if (WorldData.TryGetValue(userName, out user))
+            {
+                user.Logoff();
+            }
+        }
+
+        private void ControlMessage_MailNotifyUser(HybrasylControlMessage message)
+        {
+            // Set unread mail flag and if the user is online, send them an UpdateAttributes packet
+            var userName = (string)message.Arguments[0];
+            Logger.DebugFormat("mail: attempting to notify {0} of new mail", userName);
+            User user;
+            if (WorldData.TryGetValue(userName, out user))
+            {
+                user.UpdateAttributes(StatUpdateFlags.Secondary);
+                Logger.DebugFormat("mail: notification to {0} sent", userName);
+            }
+            else
+            {
+                Logger.DebugFormat("mail: notification to {0} failed, not logged in?", userName);
+            }
+        }
+
+        private void ControlMessage_StatusTick(HybrasylControlMessage message)
+        {
+            var userName = (string)message.Arguments[0];
+            Logger.DebugFormat("statustick: processing tick for {0}", userName);
+            User user;
+            if (WorldData.TryGetValue(userName, out user))
+            {
+                user.ProcessStatusTicks();
+            }
+            else
+            {
+                Logger.DebugFormat("tick: Cannot process tick for {0}, not logged in?", userName);
+            }
+        }
+
+        private void ControlMessage_SpawnMonster(HybrasylControlMessage message)
+        {
+            var monster = (Monster)message.Arguments[0];
+            var map = (Map)message.Arguments[1];
+            Logger.DebugFormat("monolith: spawning monster {0} on map {1}", monster.Name, map.Name);
+            map.InsertCreature(monster);
+        }
+
+        private void ControlMessage_MonolithControl(HybrasylControlMessage message)
+        {
+
+            var monster = (Monster)message.Arguments[0];
+            var map = (Map)message.Arguments[1];
+
+            if (monster.IsHostile)
+            {
+                var entityTree = map.EntityTree.GetObjects(monster.GetViewport());
+                var hasPlayer = entityTree.Any(x => x is User);
+
+                if (hasPlayer)
+                {
+                    //get players
+                    var players = entityTree.OfType<User>();
+
+                    //get closest
+                    var closest =
+                        players.OrderBy(x => Math.Sqrt((Math.Pow(monster.X - x.X, 2) + Math.Pow(monster.Y - x.Y, 2))))
+                            .FirstOrDefault();
+
+                    if (closest != null)
+                    {
+
+                        //pathfind or cast if far away
+                        var distanceX = (int)Math.Sqrt(Math.Pow(monster.X - closest.X, 2));
+                        var distanceY = (int)Math.Sqrt(Math.Pow(monster.Y - closest.Y, 2));
+                        if (distanceX >= 1 && distanceY >= 1)
+                        {
+                            var nextAction = _random.Next(1, 6);
+
+                            if (nextAction > 1)
+                            {
+                                //pathfind;
+                                if (distanceX > distanceY)
+                                {
+                                    monster.Walk(monster.X > closest.X ? Direction.West : Direction.East);
+                                }
+                                else
+                                {
+                                    //movey
+                                    monster.Walk(monster.Y > closest.Y ? Direction.North : Direction.South);
+                                }
+
+                                if (distanceX == distanceY)
+                                {
+                                    var next = _random.Next(0, 2);
+
+                                    if (next == 0)
+                                    {
+                                        monster.Walk(monster.X > closest.X ? Direction.West : Direction.East);
+                                    }
+                                    else
+                                    {
+                                        monster.Walk(monster.Y > closest.Y ? Direction.North : Direction.South);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //cast
+                                if (monster.CanCast)
+                                {
+                                    monster.Cast(closest);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //check facing and attack or cast
+
+                            var nextAction = _random.Next(1, 6);
+                            if (nextAction > 1)
+                            {
+                                var facing = monster.CheckFacing(monster.Direction, closest);
+                                if (facing)
+                                {
+                                    monster.AssailAttack(monster.Direction, closest);
+                                }
+                            }
+                            else
+                            {
+                                if (monster.CanCast)
+                                {
+                                    monster.Cast(closest);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (monster.ShouldWander)
+            {
+                var nextAction = _random.Next(0, 2);
+
+                if (nextAction == 1)
+                {
+                    var nextMove = _random.Next(0, 4);
+                    monster.Walk((Direction)nextMove);
+                }
+                else
+                {
+                    var nextMove = _random.Next(0, 4);
+                    monster.Turn((Direction)nextMove);
+                }
+            }
+        }
+
+        #endregion Control Message Handlers
+    }
+}
---
+++ hybrasyl/WorldMerchantHandlers.cs
@@ -0,0 +1,354 @@
+﻿using Hybrasyl.Enums;
+using Hybrasyl.Objects;
+using System;
+using System.Collections.Generic;
+using Castable = Hybrasyl.Castables.Castable;
+
+namespace Hybrasyl
+{
+    public partial class World
+    {
+        public void SetMerchantMenuHandlers()
+        {
+            merchantMenuHandlers = new Dictionary<MerchantMenuItem, MerchantMenuHandler>()
+            {
+                {MerchantMenuItem.MainMenu, new MerchantMenuHandler(0, MerchantMenuHandler_MainMenu)},
+                {
+                    MerchantMenuItem.BuyItemMenu,
+                    new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_BuyItemMenu)
+                },
+                //{MerchantMenuItem.BuyItem, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_BuyItem)},
+                {
+                    MerchantMenuItem.BuyItemQuantity,
+                    new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_BuyItemWithQuantity)
+                },
+                {
+                    MerchantMenuItem.BuyItemAccept, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_BuyItemAccept)
+                },
+                {
+                    MerchantMenuItem.SellItemMenu, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_SellItemMenu)
+                },
+                {
+                    MerchantMenuItem.SellItem, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_SellItem)
+                },
+                {
+                    MerchantMenuItem.SellItemQuantity, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_SellItemWithQuantity)
+                },
+                {
+                    MerchantMenuItem.SellItemConfirm, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_SellItemConfirmation)
+                },
+                {
+                    MerchantMenuItem.SellItemAccept, new MerchantMenuHandler(MerchantJob.Vend, MerchantMenuHandler_SellItemAccept)
+                },
+                {
+                    MerchantMenuItem.LearnSkillMenu, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_LearnSkillMenu)
+                },
+                {
+                    MerchantMenuItem.LearnSpellMenu, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_LearnSpellMenu)
+                },
+                {
+                    MerchantMenuItem.ForgetSkillMenu, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_ForgetSkillMenu)
+                },
+                {
+                    MerchantMenuItem.ForgetSpellMenu, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_ForgetSpellMenu)
+                },
+                {
+                    MerchantMenuItem.LearnSkill, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_LearnSkill)
+                },
+                {
+                    MerchantMenuItem.LearnSpell, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_LearnSpell)
+                },
+                {
+                    MerchantMenuItem.ForgetSkill, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_ForgetSkill)
+                },
+                {
+                    MerchantMenuItem.ForgetSpell, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_ForgetSpell)
+                },
+                {
+                    MerchantMenuItem.LearnSkillAccept, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_LearnSkillAccept)
+                },
+                {
+                    MerchantMenuItem.LearnSkillAgree, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_LearnSkillAgree)
+                },
+                {
+                    MerchantMenuItem.LearnSkillDisagree, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_LearnSkillDisagree)
+                },
+                {
+                    MerchantMenuItem.LearnSpellAccept, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_LearnSpellAccept)
+                },
+                {
+                    MerchantMenuItem.ForgetSkillAccept, new MerchantMenuHandler(MerchantJob.Skills, MerchantMenuHandler_ForgetSkillAccept)
+                },
+                {
+                    MerchantMenuItem.ForgetSpellAccept, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_ForgetSpellAccept)
+                },
+                {
+                    MerchantMenuItem.LearnSpellAgree, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_LearnSpellAgree)
+                },
+                {
+                    MerchantMenuItem.LearnSpellDisagree, new MerchantMenuHandler(MerchantJob.Spells, MerchantMenuHandler_LearnSpellDisagree)
+                },
+                {
+                    MerchantMenuItem.SendParcelMenu, new MerchantMenuHandler(MerchantJob.Post, MerchantMenuHandler_SendParcelMenu)
+                },
+                {
+                    MerchantMenuItem.SendParcelAccept, new MerchantMenuHandler(MerchantJob.Post, MerchantMenuHandler_SendParcelAccept)
+                },
+                {
+                    MerchantMenuItem.SendParcel, new MerchantMenuHandler(MerchantJob.Post, MerchantMenuHandler_SendParcel)
+                },
+                {
+                    MerchantMenuItem.SendParcelRecipient, new MerchantMenuHandler(MerchantJob.Post, MerchantMenuHandler_SendParcelRecipient)
+                },
+                {
+                    MerchantMenuItem.SendParcelFailure, new MerchantMenuHandler(MerchantJob.Post, MerchantMenuHandler_SendParcelFailure)
+                },
+
+            };
+        }
+        #region Merchant Menu ItemObject Handlers
+
+        private void MerchantMenuHandler_MainMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            merchant.DisplayPursuits(user);
+        }
+
+        private void MerchantMenuHandler_BuyItemMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowBuyMenu(merchant);
+        }
+
+        private void MerchantMenuHandler_SellItemMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowSellMenu(merchant);
+        }
+
+        private void MerchantMenuHandler_BuyItem(User user, Merchant merchant, ClientPacket packet)
+        {
+            //this is no longer used
+            string name = packet.ReadString8();
+
+            var template = merchant.Inventory[name];
+
+            if (template.Stackable)
+            {
+                user.ShowBuyMenuQuantity(merchant, name);
+                return;
+            }
+
+            if (user.Gold < template.Properties.Physical.Value)
+            {
+                user.ShowMerchantGoBack(merchant, "You do not have enough gold.", MerchantMenuItem.BuyItemMenu);
+                return;
+            }
+
+            if (user.CurrentWeight + template.Properties.Physical.Weight > user.MaximumWeight)
+            {
+                user.ShowMerchantGoBack(merchant, "That item is too heavy for you to carry.",
+                    MerchantMenuItem.BuyItemMenu);
+                return;
+            }
+
+            if (user.Inventory.IsFull)
+            {
+                user.ShowMerchantGoBack(merchant, "You cannot carry any more items.", MerchantMenuItem.BuyItemMenu);
+                return;
+            }
+
+            user.RemoveGold(template.Properties.Physical.Value);
+            var item = CreateItem(template.Id);
+            Insert(item);
+            user.AddItem(item);
+
+            user.UpdateAttributes(StatUpdateFlags.Experience);
+            user.ShowBuyMenu(merchant);
+        }
+
+        private void MerchantMenuHandler_BuyItemWithQuantity(User user, Merchant merchant, ClientPacket packet)
+        {
+            string name = packet.ReadString8();
+            string qStr = packet.ReadString8();
+
+            user.ShowBuyMenuQuantity(merchant, name);
+        }
+
+        private void MerchantMenuHandler_BuyItemAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowBuyItem(merchant);
+        }
+
+        private void MerchantMenuHandler_SellItem(User user, Merchant merchant, ClientPacket packet)
+        {
+            byte slot = packet.ReadByte();
+
+            var item = user.Inventory[slot];
+
+            if (item.Stackable && item.Count > 1)
+            {
+                user.ShowSellQuantity(merchant, slot);
+                return;
+            }
+
+            user.ShowSellConfirm(merchant, slot);
+        }
+
+        private void MerchantMenuHandler_SellItemWithQuantity(User user, Merchant merchant, ClientPacket packet)
+        {
+            byte slot = packet.ReadByte();
+            byte quantity = packet.ReadByte();
+
+
+            if (quantity < 1)
+            {
+                user.ShowSellQuantity(merchant, slot);
+                return;
+            }
+
+            var item = user.Inventory[slot];
+            if (item == null || !item.Stackable) return;
+
+            user.ShowSellConfirm(merchant, slot, quantity);
+        }
+
+        private void MerchantMenuHandler_SellItemConfirmation(User user, Merchant merchant, ClientPacket packet)
+        {
+            packet.ReadByte();
+            byte slot = packet.ReadByte();
+            byte quantity = packet.ReadByte();
+
+            var item = user.Inventory[slot];
+            if (item == null) return;
+
+            if (!merchant.Inventory.ContainsKey(item.Name))
+            {
+                user.ShowMerchantGoBack(merchant, "I do not want that item.", MerchantMenuItem.SellItemMenu);
+                return;
+            }
+
+            if (item.Count < quantity)
+            {
+                user.ShowMerchantGoBack(merchant, "You don't have that many to sell.", MerchantMenuItem.SellItemMenu);
+                return;
+            }
+
+            uint profit = (uint)(Math.Round(item.Value * 0.50) * quantity);
+
+            if (item.Stackable && quantity < item.Count)
+                user.DecreaseItem(slot, quantity);
+            else user.RemoveItem(slot);
+
+            user.AddGold(profit);
+
+            merchant.DisplayPursuits(user);
+        }
+
+        private void MerchantMenuHandler_SellItemAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.SellItemAccept(merchant);
+        }
+
+        private void MerchantMenuHandler_LearnSkillMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSkillMenu(merchant);
+        }
+        private void MerchantMenuHandler_LearnSkill(User user, Merchant merchant, ClientPacket packet)
+        {
+            var skillName = packet.ReadString8(); //skill name
+            var skill = WorldData.GetByIndex<Castable>(skillName);
+            user.ShowLearnSkill(merchant, skill);
+        }
+        private void MerchantMenuHandler_LearnSkillAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSkillAccept(merchant);
+        }
+
+        private void MerchantMenuHandler_LearnSkillAgree(User user, Merchant merchant, ClientPacket packet)
+        {
+
+            user.ShowLearnSkillAgree(merchant);
+        }
+
+        private void MerchantMenuHandler_LearnSkillDisagree(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSkillDisagree(merchant);
+        }
+
+        private void MerchantMenuHandler_LearnSpellMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSpellMenu(merchant);
+        }
+        private void MerchantMenuHandler_LearnSpell(User user, Merchant merchant, ClientPacket packet)
+        {
+            var spellName = packet.ReadString8();
+            var spell = WorldData.GetByIndex<Castable>(spellName);
+            user.ShowLearnSpell(merchant, spell);
+        }
+        private void MerchantMenuHandler_LearnSpellAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSpellAccept(merchant);
+        }
+        private void MerchantMenuHandler_LearnSpellAgree(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSpellAgree(merchant);
+        }
+        private void MerchantMenuHandler_LearnSpellDisagree(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowLearnSpellDisagree(merchant);
+        }
+
+        private void MerchantMenuHandler_ForgetSkillMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowForgetSkillMenu(merchant);
+        }
+        private void MerchantMenuHandler_ForgetSkill(User user, Merchant merchant, ClientPacket packet)
+        {
+
+        }
+        private void MerchantMenuHandler_ForgetSkillAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            var slot = packet.ReadByte();
+
+            user.ShowForgetSkillAccept(merchant, slot);
+        }
+        private void MerchantMenuHandler_ForgetSpellMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowForgetSpellMenu(merchant);
+        }
+        private void MerchantMenuHandler_ForgetSpell(User user, Merchant merchant, ClientPacket packet)
+        {
+
+        }
+        private void MerchantMenuHandler_ForgetSpellAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            var slot = packet.ReadByte();
+
+            user.ShowForgetSpellAccept(merchant, slot);
+        }
+
+        private void MerchantMenuHandler_SendParcelMenu(User user, Merchant merchant, ClientPacket packet)
+        {
+            user.ShowMerchantSendParcel(merchant);
+        }
+        private void MerchantMenuHandler_SendParcelRecipient(User user, Merchant merchant, ClientPacket packet)
+        {
+            var item = packet.ReadByte();
+            var itemObj = user.Inventory[item];
+            user.ShowMerchantSendParcelRecipient(merchant, itemObj);
+        }
+        private void MerchantMenuHandler_SendParcel(User user, Merchant merchant, ClientPacket packet)
+        {
+
+        }
+
+        private void MerchantMenuHandler_SendParcelFailure(User user, Merchant merchant, ClientPacket packet)
+        {
+
+        }
+        private void MerchantMenuHandler_SendParcelAccept(User user, Merchant merchant, ClientPacket packet)
+        {
+            var recipient = packet.ReadString8();
+            user.ShowMerchantSendParcelAccept(merchant, recipient);
+        }
+        #endregion Merchant Menu ItemObject Handlers
+    }
+}
