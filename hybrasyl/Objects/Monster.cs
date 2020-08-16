fcacd6ea838e8a6e9077b540e385a7035b04c270
Justin Baugh <baughj@hybrasyl.com>
2020-08-16 14:13:32 -0700

    simplify script checks for onhear

--- hybrasyl/Objects/Monster.cs
+++ hybrasyl/Objects/Monster.cs
@@ -52,7 +52,7 @@ namespace Hybrasyl.Objects
         public bool MovementDisabled => _spawn.Flags.HasFlag(Xml.SpawnFlags.MovementDisabled);
         public bool AiDisabled => _spawn.Flags.HasFlag(Xml.SpawnFlags.AiDisabled);
 
-        public bool ScriptDisabled { get; set; }
+        public bool ScriptExists { get; set; }
 
         public override void OnDeath()
         {
@@ -112,17 +112,17 @@ namespace Hybrasyl.Objects
         // OnSpawn) when not needed 99% of the time.
         private void InitScript()
         {
-            if (Script != null || ScriptDisabled == true)
+            if (Script != null || !ScriptExists)               
                 return;
 
             if (World.ScriptProcessor.TryGetScript(Name, out Script damageScript))
             {
                 Script = damageScript;
                 Script.AssociateScriptWithObject(this);
-                ScriptDisabled = false;
+                ScriptExists = true;
             }
             else
-                ScriptDisabled = true;                
+                ScriptExists = false;                
         }
 
         public override void OnHear(VisibleObject speaker, string text, bool shout = false)
@@ -132,7 +132,7 @@ namespace Hybrasyl.Objects
 
             // FIXME: in the glorious future, run asynchronously with locking
             InitScript();
-            if (Script != null && !ScriptDisabled)
+            if (Script != null)
             {
                 Script.SetGlobalValue("text", text);
                 Script.SetGlobalValue("shout", shout);
@@ -148,7 +148,8 @@ namespace Hybrasyl.Objects
         {
             // FIXME: in the glorious future, run asynchronously with locking
             InitScript();
-            if (Script != null && !ScriptDisabled)
+
+            if (Script != null)
             {
                 Script.SetGlobalValue("damage", damage);
                 Script.ExecuteFunction("OnDamage", this, attacker);
@@ -159,7 +160,7 @@ namespace Hybrasyl.Objects
         {
             // FIXME: in the glorious future, run asynchronously with locking
             InitScript();
-            if (Script != null && !ScriptDisabled)
+            if (Script != null)
             {
                 Script.SetGlobalValue("heal", heal);
                 Script.ExecuteFunction("OnHeal", this, healer);
