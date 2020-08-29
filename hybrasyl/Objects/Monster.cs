a117a0c18267a093a4eead8f47b4ebcd090c6e99
Michael Norris <norrismiv@gmail.com>
2020-08-29 13:46:08 -0400

    fix for aggro to prevent wander and make hostile

--- hybrasyl/Objects/Monster.cs
+++ hybrasyl/Objects/Monster.cs
@@ -183,6 +183,8 @@ namespace Hybrasyl.Objects
             {
                 AggroTable[attacker.Id] += damage;
             }
+            IsHostile = true;
+            ShouldWander = false;
 
             // FIXME: in the glorious future, run asynchronously with locking
             InitScript();
