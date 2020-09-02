9361cc776b9c4b682977adcffa1cd9517c436dbf
Justin Baugh <baughj@hybrasyl.com>
2020-09-02 11:54:27 -0700

    fix IndexOutOfRange exception

--- hybrasyl/Objects/Monster.cs
+++ hybrasyl/Objects/Monster.cs
@@ -657,8 +657,7 @@ namespace Hybrasyl.Objects
 
             (int x, int y) point = NextPoint(direction, currentPoint);
 
-
-            if(!Map.IsWall[point.x, point.y] && !invalidPoints.Contains(point))
+            if(point.x <= Map.X && point.y <= Map.Y && !Map.IsWall[point.x, point.y] && !invalidPoints.Contains(point))
             {
                 Walk(direction);
             }
