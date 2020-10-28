602b43ebb92f47bd58c5269944a80f51fa6f504b
Justin Baugh <baughj@eris.co>
2020-10-28 09:30:09 -0700

    add behaviorset types; update xml defuckery script

--- XML/xsd-fix.sh
+++ XML/xsd-fix.sh
@@ -6,19 +6,24 @@
 set -e -x
 
 XSDPATH="XSD/Objects"
-OLD='"4\.8\.4084\.0"'
-NEW='"4\.8"'
 
-for x in $(ls ${XSDPATH}/*.cs); do
-    sed -i -e "s/${OLD}/${NEW}/g;" $x
-done
+#OLD='"4\.8\.4084\.0"'
+#NEW='"4\.8"'
+
+# This is no longer necessary now that people that don't know how to use tooling are
+# no longer working on the project
+
+#for x in $(ls ${XSDPATH}/*.cs); do
+#    sed -i -e "s/${OLD}/${NEW}/g;" $x
+#done
 
 git checkout ${XSDPATH}/Castable.cs
 git checkout ${XSDPATH}/ElementTableSourceElement.cs
 git checkout ${XSDPATH}/ElementTableTargetElement.cs
 git checkout ${XSDPATH}/EquipmentRestriction.cs
-git checkout ${XSDPATH}/SpawnCastable.cs
-git checkout ${XSDPATH}/SpawnCastableDefense.cs
-git checkout ${XSDPATH}/SpawnCastableOffense.cs
+git checkout ${XSDPATH}/SpawnDamage.cs
+git checkout ${XSDPATH}/SpawnDefense.cs
 git checkout ${XSDPATH}/StatModifiers.cs
+git checkout ${XSDPATH}/StatModifierFormulas.cs
+
 
