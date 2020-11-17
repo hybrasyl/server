f61794be44a627a9d0ed7302f1315462fe3c46c9
Justin Baugh <baughj@eris.co>
2020-11-17 11:35:23 -0800

    comment out offending structures; savegame

---
+++ XML/XSD/Extensions/SpawnGroup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hybrasyl.Xml
+{
+    public partial class SpawnGroup : HybrasylLoadable, IHybrasylLoadable<SpawnGroup>
+    {
+
+    }
+}
