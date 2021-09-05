8ca279949308811892194db178380bbaa7ea2405
Justin Baugh <justin.baugh@eris.co>
2021-09-05 13:56:13 -0700

    re-enable monster info

--- hybrasyl/Scripting/HybrasylMonster.cs
+++ hybrasyl/Scripting/HybrasylMonster.cs
@@ -44,60 +44,55 @@ namespace Hybrasyl.Scripting
             var s = "Monster Debug Info\n----------------------------\n\n";
 
             ////this is for debug only
-            //s += $"Name: {Monster.Name} | Id: {Monster.Id}\n";
-            //s += $"Level: {Monster.Stats.Level} Health: {Monster.Stats.Hp}/{Monster.Stats.MaximumHp}\n";
-            //s += $"Damage: {Monster._spawn.Damage.Min}-{Monster._spawn.Damage.Max}\n";
-            //s += $"Experience: {Monster._spawn.Loot.Xp}\n";
-            //s += $"Castables:\n";
-            //s += $"  Offense: ({Monster._spawn.Castables.Offense.Interval} second timer) \n";
-            //foreach (var castable in Monster._spawn.Castables.Offense.Castables)
-            //{
-            //    s += $"    {castable.Name}:\n";
-            //    s += $"      Damage: {castable.MinDmg}-{castable.MaxDmg}\n";
-            //    s += $"      Element: {castable.Element}\n";
-            //    s += $"      TargetType: {castable.Target.ToString()}\n";
-            //}
-            //s += $"  Defense: ({Monster._spawn.Castables.Defense.Interval} second timer) \n";
-            //foreach (var castable in Monster._spawn.Castables.Defense.Castables)
-            //{
-            //    s += $"    {castable.Name}:\n";
-            //    s += $"      Damage: {castable.MinDmg}-{castable.MaxDmg}\n";
-            //    s += $"      Element: {castable.Element}\n";
-            //    s += $"      TargetType: {castable.Target.ToString()}\n";
-            //}
-            //s += $"  NearDeath: ({Monster._spawn.Castables.NearDeath.Interval} second timer) \n";
-            //foreach (var castable in Monster._spawn.Castables.NearDeath.Castables)
-            //{
-            //    s += $"    {castable.Name}:\n";
-            //    s += $"      Damage: {castable.MinDmg}-{castable.MaxDmg}\n";
-            //    s += $"      Element: {castable.Element}\n";
-            //    s += $"      TargetType: {castable.Target.ToString()}\n";
-            //}
-            //s += $"  OnDeath:\n";
-            //foreach (var castable in Monster._spawn.Castables.OnDeath)
-            //{
-            //    s += $"    {castable.Name}:\n";
-            //    s += $"      Damage: {castable.MinDmg}-{castable.MaxDmg}\n";
-            //    s += $"      Element: {castable.Element}\n";
-            //    s += $"      TargetType: {castable.Target.ToString()}\n";
-            //}
-            //s += $"AbsoluteImmortal: {Monster.AbsoluteImmortal}\n";
-            //s += $"PhysicalImmortal: {Monster.PhysicalImmortal}\n";
-            //s += $"MagicalImmortal: {Monster.MagicalImmortal}\n";
-            //s += $"IsHostile: {Monster.IsHostile}\n";
-            //s += $"ShouldWander: {Monster.ShouldWander}\n";
-            ////if (Monster.Target != 0) s += $"Target: {Monster.Target.Name}\n";
-            //if (Monster.FirstHitter != null) s += $"FirstHitter: {Monster.FirstHitter.Name}\n";
-            ////if (Monster.LastHitter != null) s += $"LastHitter: {Monster.LastHitter.Name}\n";
-            //if (Monster.LastHitTime != null) s += $"LastHitTime: {Monster.LastHitTime}\n";
-            //if (Monster.ThreatInfo != null)
-            //{
-            //    s += $"ThreatInfo:\n";
-            //    foreach(var user in Monster.ThreatInfo.ThreatTable)
-            //    {
-            //        s += $"Name: {user.Key.Name} | Threat: {user.Value}\n";
-            //    }
-            //}
+           s += $"Name: {Monster.Name} | Id: {Monster.Id}\n";
+           s += $"Level: {Monster.Stats.Level}  Health: {Monster.Stats.Hp}/{Monster.Stats.MaximumHp}  Mana: {Monster.Stats.Mp} / {Monster.Stats.MaximumMp}\n";
+           s += $"Experience: {Monster.LootableXP}\n";
+           s += $"Castables:\n";
+            if (Monster.BehaviorSet?.Behavior?.Casting?.Offense != null)
+            {
+                s += $"  Offense: ({Monster.BehaviorSet.Behavior.Casting.Offense.Interval} second timer) \n";
+                s += $"  {String.Join(',', Monster.BehaviorSet.OffensiveCastables)}";
+                s += $"  Target: {Monster.BehaviorSet.Behavior.Casting.Offense.Priority}";
+            }
+            if (Monster.BehaviorSet?.Behavior?.Casting?.Defense != null)
+            {
+                s += $"  Offense: ({Monster.BehaviorSet.Behavior.Casting.Defense.Interval} second timer) \n";
+                s += $"  {String.Join(',', Monster.BehaviorSet.DefensiveCastables)}";
+                s += $"  Target: {Monster.BehaviorSet.Behavior.Casting.Defense.Priority}";
+            }
+
+            if (Monster.BehaviorSet?.Behavior?.Casting?.NearDeath != null)
+            {
+                s += $"  Offense: ({Monster.BehaviorSet.Behavior.Casting.NearDeath.Interval} second timer) \n";
+                s += $"  {String.Join(',', Monster.BehaviorSet.NearDeathCastables)}";
+                s += $"  Target: {Monster.BehaviorSet.Behavior.Casting.NearDeath.Priority}";
+            }
+
+            if (Monster.BehaviorSet?.Behavior?.Casting?.OnDeath != null)
+            {
+                s += $"  Offense: ({Monster.BehaviorSet.Behavior.Casting.OnDeath.Interval} second timer) \n";
+                s += $"  {String.Join(',', Monster.BehaviorSet.OnDeathCastables)}";
+                s += $"  Target: {Monster.BehaviorSet.Behavior.Casting.OnDeath.Priority}";
+            }
+
+            s += $"AbsoluteImmortal: {Monster.AbsoluteImmortal}\n";
+            s += $"PhysicalImmortal: {Monster.PhysicalImmortal}\n";
+            s += $"MagicalImmortal: {Monster.MagicalImmortal}\n";
+            s += $"IsHostile: {Monster.IsHostile}\n";
+            s += $"ShouldWander: {Monster.ShouldWander}\n";
+
+            if (Monster.Target != null) s += $"Target: {Monster.Target.Name}\n";
+            if (Monster.FirstHitter != null) s += $"FirstHitter: {Monster.FirstHitter.Name}\n";
+            if (Monster.LastHitter != null) s += $"LastHitter: {Monster.LastHitter.Name}\n";
+            if (Monster.LastHitTime != default) s += $"LastHitTime: {Monster.LastHitTime}\n";
+            if (Monster.ThreatInfo != null)
+            {
+                s += $"ThreatInfo:\n";
+                foreach(var user in Monster.ThreatInfo.ThreatTableByCreature)
+                {
+                    s += $"Name: {user.Key.Name} | Threat: {user.Value}\n";
+                }
+            }
 
             return s;
         }
